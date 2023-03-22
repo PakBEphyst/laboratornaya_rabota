13e3c41da8f92072f09189e74df4e58ba65f1b36
razvitayaobeziana <114497939+razvitayaobeziana@users.noreply.github.com>
2023-03-23 00:29:49 +0300

    заголовочный файл функций

---
+++ func.h
@@ -0,0 +1,10 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+bool search_line_sum (int* arr, int n, int sum);
+
+bool search_line (int* beg_ptr, int n, int elem);
+
+bool search_bin (int* beg_ptr, int n, int elem);
+
+#endif
